c86d14d72a2fb734e9bfe3433c65769c01e5893e
Kelper <644050173@qq.com>
2019-07-23 21:30:40 +0800

    Create A1002.cpp

---
+++ A1002.cpp
@@ -0,0 +1,31 @@
+#include<cstdio> 
+const int max_n = 1111;
+double p[max_n] = {};
+int main() {
+	int k,n, count = 0;
+	double a;
+	scanf("%d", &k);
+	while(k--) {
+		scanf("%d %lf", &n, &a);
+		p[n] += a;
+	}
+	
+	scanf("%d", &k);
+	while(k--) {
+		scanf("%d %lf", &n, &a);
+		p[n] += a;
+	}
+	
+	for(int i = 0; i < max_n; i++) {
+		if(p[i] != 0) {
+			count++;
+		}
+	}
+	printf("%d", count);
+	for(int i = 0; i < max_n; i++) {
+		if(p[i] != 0) {
+			printf(" %d %.1f", i, p[i]);
+		}
+	}
+	return 0;
+}
