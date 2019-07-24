58f1f997d885373759a2aa125c0657e20d5807f8
Kelper <644050173@qq.com>
2019-07-24 21:30:34 +0800

    Create A1036.cpp

---
+++ A1036.cpp
@@ -0,0 +1,48 @@
+#include<cstdio>
+struct person{
+	char name[15];
+	char id[15];
+	int score;
+}M, F, temp;
+
+//初始化结构体
+void init() {
+	M.score = 101;
+	F.score = -1;
+} 
+int main() {
+	init();
+	int n;
+	char gender;
+	scanf("%d", &n);
+	
+	for(int i = 0; i < n; i++) {
+		scanf("%s %c %s %d", &temp.name, &gender, &temp.id, &temp.score);
+		if(gender == 'M' && temp.score < M.score) {
+			M = temp;
+		}
+		
+		if(gender == 'F' && temp.score > F.score) {
+			F = temp;
+		}
+	}
+	
+	if (F.score == -1) {
+		printf("Absent\n");
+	} else {
+		printf("%s %s\n", F.name, F.id);
+	}
+	
+	if (M.score == 101) {
+		printf("Absent\n");
+	} else {
+		printf("%s %s\n", M.name, M.id);
+	}
+	
+	if (M.score == 101 || F.score == -1) {
+		printf("NA");
+	} else {
+		printf("%d\n", F.score - M.score);
+	}
+	return 0;
+} 
