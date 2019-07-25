1e39a91d3e00825ccf5feead0906cb7e2faaee6b
Kelper <644050173@qq.com>
2019-07-25 20:26:14 +0800

    Create B1037.cpp

---
+++ B1037.cpp
@@ -0,0 +1,20 @@
+#include<cstdio> 
+const int Galleon = 17 * 29;
+const int Sickle = 29;
+// 进制转化 和时间转化没什么区别 
+int main() {
+	int a1, b1, c1;
+	int a2, b2, c2;
+	scanf("%d.%d.%d %d.%d.%d", &a1, &b1, &c1, &a2, &b2, &c2);
+	int price = a1 * Galleon + b1 * Sickle + c1;
+	int money = a2 * Galleon + b2 * Sickle + c2;
+	
+	int change = money - price;
+	if (change < 0) {
+		printf("-");
+		change = -change;
+	}
+	
+	printf("%d.%d.%d\n", change / Galleon, change % Galleon / Sickle, change % Sickle);
+	return 0;
+}
