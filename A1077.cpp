af1d3e31523e3103a4d02ba473c9ccd36f83221f
Kelper <644050173@qq.com>
2019-07-28 19:23:52 +0800

    转cpp

--- A1077.cpp
+++ A1077.cpp
@@ -1,46 +1,32 @@
+#include<iostream>
+#include<algorithm>
 #include<cstdio>
-#include<cstring>
-char str[100][256];
-int n, minLen = 256, ans = 0;
+using namespace std;
 int main() {
-	scanf("%d", &n);
-	getchar();
-	
-	
+	int n;
+	scanf("%d\n", &n);
+	string ans;
 	for (int i = 0; i < n; i++) {
-		fgets(str[i], 258, stdin);
-		int len = strlen(str[i]);
-		if (len < minLen) {
-			minLen = len;
-		}
-		
-		for (int j = 0; j < len / 2; j++) {
-			char temp = str[i][j];
-			str[i][j] = str[i][len - 1 - j];
-			str[i][len - 1 - j] = temp;
-		}
-	} 
-	
-	for (int i = 0; i < minLen; i++) {
-		char c = str[0][i];
-		bool same = true;
-		for (int j = 1; j < n; j++) {
-			if (c != str[j][i]) {
-				same = false;
-				break;
+		string s;
+		getline(cin, s);
+		int lens = s.length();
+		reverse(s.begin(), s.end());
+		if(i == 0) {
+			ans = s;
+			continue;
+		} else {
+			int lenans = ans.length();
+			int minlen = min(lens, lenans);
+			for (int j = 0; j < minlen; j++) {
+				if (ans[j] != s[j]) {
+					ans = ans.substr(0, j);
+					break;
+				}
 			}
 		}
-		if(same) ans++;
-		else break;
-	} 
-	
-	if (ans) {
-		for (int i = ans - 1; i >= 0; i--) {
-			printf("%c", str[0][i]);
-		} 
-	} else {
-		printf("nai");
 	}
-	
+	reverse(ans.begin(), ans.end());
+	if(ans.length() == 0) ans = "nai";
+	cout << ans;
 	return 0;
 } 
