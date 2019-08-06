bce7faf4f710561e8fb3c14171ab47aee9ffa563
Kelper <644050173@qq.com>
2019-08-06 18:52:57 +0800

    Update A1048.cpp

--- A1048.cpp
+++ A1048.cpp
@@ -1,26 +1,38 @@
 #include<cstdio>
+#include<iostream>
 #include<algorithm>
 using namespace std;
-const int N = 10000;
-int hashTable[N];
-int main() {
-	int n, m, a;
-	scanf("%d %d", &n, &m);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &a);
-		hashTable[a]++;
+int a[100010];
+
+// left right 0 n - 1 key is m - a[i] 
+int Bin(int left, int right, int key) {
+	int mid;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (a[mid] == key) return mid;
+		else if (a[mid] > key) right = mid - 1;
+		else left = mid + 1;
 	}
-	for (int i = 1; i < m; i++) {
-		if (hashTable[i] && hashTable[m - i]) {
-			if (i == m -i && hashTable[i] <= 1) {
-				continue;
-			}
-            printf("%d %d\n",i, m - i);
-		return 0;
-		}
-		
+	return -1;
+}
+
+int main() {
+	int i, n, m;
+	scanf("%d%d", &n, &m);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
 	}
 	
-	printf("No Solution");
+	sort(a, a + n);
+	for (i = 0; i < n; i++) {
+		int pos = Bin(0, n - i, m - a[i]);
+		if (pos != -1 && i != pos) {
+			printf("%d %d\n", a[i], a[pos]);
+			break;
+		}
+	} 
+	
+	if (i == n) printf("No Solution");
 	return 0;
 }
+
