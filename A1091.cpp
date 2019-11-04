965c777788b601830136db6909583ee1720e01b9
libin <644050173@qq.com>
2019-11-05 00:03:02 +0800

    1091 Acute Stroke (30 分)

--- A1091.cpp
+++ A1091.cpp
@@ -1,10 +1,9 @@
-#include<cstdio>
-#include<queue>
+#include <cstdio>
+#include <queue>
 using namespace std;
 struct node {
-	int x, y, z;
-} Node; 
-
+    int x, y, z;
+} Node;
 int n, m, slice, T;
 int pixel[1290][130][61];
 bool inq[1290][130][61] = {false};
@@ -13,64 +12,65 @@ int Y[6] = {0, 0, 1, -1, 0, 0};
 int Z[6] = {1, -1, 0, 0, 0, 0};
 
 bool judge(int x, int y, int z) {
-	// 越界 
-	if (x >= n || x < 0 || y >= m || y < 0 || z >= slice || z < 0) {
-		return false;
-	}
-	//当前位置为0 或者已经入队 则返回false
-	if (pixel[x][y][z] == 0 || inq[x][y][z] == true) {
-		return false;
-	} 
-	return true;
+    // 瓒婄晫
+    if (x >= n || x < 0 || y >= m || y < 0 || z >= slice || z < 0) {
+        return false;
+    }
+    if (pixel[x][y][z] == 0 || inq[x][y][z] == true) {
+        return false;
+    }
+    return true;
 }
 
 int BFS(int x, int y, int z) {
-	int tot = 0; // 定义当前块中1的个数 
-	queue<node> q; // 定义队列
-	Node.x = x, Node.y = y, Node.z = z;
-	q.push(Node); // 将节点入队
-	inq[x][y][z] = true;
-	
-	while(!q.empty()) {
-		node top = q.front(); // 取出队首元素
-		q.pop(); // 队首元素出队 
-		tot++; // 当前块中1的个数加一
-		 
-		for (int i = 0; i < 6; i++) {
-			int newX = top.x + X[i];
-			int newY = top.y + Y[i];
-			int newZ = top.z + Z[i];
-			
-			if (judge(newX, newY, newZ)) {
-				Node.x = newX, Node.y = newY, Node.z = newZ;
-				q.push(Node);
-				inq[newX][newY][newZ] = true; 
-			}
-		} 
-	}
-	
-	if (tot >= T) return tot;
-	else return 0; 
+    int tot = 0;
+    queue<node> q;
+    Node.x = x, Node.y = y, Node.z = z;
+    q.push(Node);
+    inq[x][y][z] = true;
+    while (!q.empty()) {
+        node top = q.front();
+        q.pop();
+        tot++; // 鍧楁暟鍔犱竴
+        for (int i = 0; i < 6; ++i) {
+            int newX = top.x + X[i];
+            int newY = top.y + Y[i];
+            int newZ = top.z + Z[i];
+            if (judge(newX, newY, newZ)) {
+                Node.x = newX;
+                Node.y = newY;
+                Node.z = newZ;
+                // 灏哊ode鑺傜偣鍏ラ槦
+                q.push(Node);
+                inq[newX][newY][newZ] = true;
+            }
+        }
+    }
+    if (tot >= T) {
+        return tot;
+    } else {
+        return 0;
+    }
 }
 int main() {
-	scanf("%d%d%d%d", &n, &m, &slice, &T);
-	for (int z = 0; z < slice; z++) {
-		for (int x = 0; x < n; x++) {
-			for (int y = 0; y < m; y++) {
-				scanf("%d", &pixel[x][y][z]);
-			}
-		}
-	}
-	int ans = 0;
-		for (int z = 0; z < slice; z++) {
-		for (int x = 0; x < n; x++) {
-			for (int y = 0; y < m; y++) {
-				if (pixel[x][y][z] == 1 && inq[x][y][z] == false) {
-					ans += BFS(x, y, z);
-				}
-			}
-		}
-	}
-	printf("%d\n", ans);
-	return 0;
+    scanf("%d%d%d%d", &n, &m, &slice, &T);
+    for (int z = 0; z < slice; ++z) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                scanf("%d", &pixel[i][j][z]);
+            }
+        }
+    }
+    int ans = 0;
+    for (int z = 0; z < slice; ++z) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (pixel[i][j][z] == 1 && inq[i][j][z] == false) {
+                    ans += BFS(i, j, z);
+                }
+            }
+        }
+    }
+    printf("%d", ans);
+    return 0;
 }
