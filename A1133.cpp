7b8814b2455e06f50b9b74baacc6365757eaa508
kelper <644050173@qq.com>
2020-05-02 21:28:14 +0800

    1133 Splitting A Linked List (25 分)

--- A1133.cpp
+++ A1133.cpp
@@ -1,58 +1,56 @@
 //
-// Created by Kelper on 2019/11/3.
+// Created by kelper on 2020/5/2.
 //
-#include <cstdio>
+#include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
-int n, k, p, maxFacSum = -1;
-vector<int> fac, ans, temp;
-int power(int x) {
-    int ans = 1;
-    for (int i = 0; i < p; ++i) {
-        ans *= x;
-    }
-    return ans;
-}
-
-void init() {
-    int i = 0, temp = 0;
-    while (temp <= n) {
-        fac.push_back(temp);
-        temp = power(++i);
-    }
-}
-
-void DFS(int index, int nowK, int sum, int facSum) {
-    if (sum == n && nowK == k) {
-        if (facSum > maxFacSum) {
-            ans = temp;
-            maxFacSum = facSum;
-        }
-        return;
-    }
-    if (sum > n || nowK > k) {
-        return;
-    }
-    if (index - 1 >= 0) {
-        temp.push_back(index);
-        DFS(index, nowK + 1, sum + fac[index], facSum + index);
-        temp.pop_back();
-        DFS(index - 1, nowK, sum, facSum);
-    }
-}
+struct node {
+    int id, data, next;
+};
 
 int main() {
-    scanf("%d%d%d", &n, &k, &p);
-    init();
-    DFS(fac.size() - 1, 0, 0, 0);
-    if (maxFacSum == -1) {
-        printf("Impossible");
-    } else {
-        printf("%d = %d^%d", n, ans[0], p);
-        for (int i = 1; i < ans.size(); ++i) {
-            printf(" + %d^%d", ans[i], p);
+    int begin, n, k;
+    int adress, data, next;
+    scanf("%d %d %d", &begin, &n, &k);
+    node a[100010];
+    vector<node> v, ans;
+    for (int i = 0; i < n; ++i) {
+        scanf("%d %d %d", &adress, &data, &next);
+        a[adress] = {adress, data, next};
+    }
+
+    // 组装链表
+    while (begin != - 1) {
+        v.push_back(a[begin]);
+        begin = a[begin].next;
+    }
+
+    // 按段处理
+    // 1.小于0
+    for (int j = 0; j < v.size(); ++j) {
+        if (v[j].data < 0) {
+            ans.push_back(v[j]);
         }
     }
+    // 2.大于等于0 小于等于k
+    for (int l = 0; l < v.size(); ++l) {
+        if (v[l].data >= 0 && v[l].data <= k) {
+            ans.push_back(v[l]);
+        }
+    }
+    //3.大于k
+    for (int m = 0; m < v.size(); ++m) {
+        if (v[m].data > k) {
+            ans.push_back(v[m]);
+        }
+    }
+
+    // 输出
+    for (int i = 0; i < ans.size() - 1; ++i) {
+        // 新的链表
+        // 下一个就是ans[i+1].id
+        printf("%05d %d %05d\n", ans[i].id, ans[i].data, ans[i + 1].id);
+    }
+    printf("%05d %d -1", ans[ans.size() - 1].id, ans[ans.size() - 1].data);
     return 0;
 }
