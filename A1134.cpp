3f4412b10d4aafe5c6982e5f23cbd093d55de4ac
kelper <644050173@qq.com>
2020-06-19 09:17:35 +0800

    1134 Vertex Cover (25分)

--- A1134.cpp
+++ A1134.cpp
@@ -1,61 +1,46 @@
-//
-// Created by kelper on 2020/5/3.
-//
+#include <bits/stdc++.h>
 
-#include <iostream>
-#include <vector>
 using namespace std;
-// 给点构图
-// 用点集S查询 是否与所有边有关系
-// 必须建立点和边的关系
-//    v[a].push_back(i);
-//    v[b].push_back(i);
 
-// 扫描某个点的边集合
-// 进行标记
-// 如果所有的边被标记了 则符合题目要求
 int main() {
     int n, m, k;
-    scanf("%d %d",&n, &m);
+    cin >> n >> m;
+    unordered_map<int,vector<int>> mp;
+    int a,  b;
+    for (int i = 1; i <= m; ++i) {
 
-    // 点集
-    vector<int > v[n];
-    // 用点描述边
-    int a, b;
-    for(int i = 0; i < m; ++i) {
-        scanf("%d %d", &a, &b);
-        v[a].push_back(i);
-        v[b].push_back(i);
+//        cin >> a >> b;
+        scanf("%d %d",&a,&b);
+        mp[a].push_back(i);
+        mp[b].push_back(i);
     }
+    cin >> k;
+    int num;
+    int nv;
+    for (int i = 0; i < k; ++i) {
 
-    int nv, num;
-    // 做查询
-    scanf("%d", &k);
-    for (int j = 0; j < k; ++j) {
-        scanf("%d", &nv);
+//        cin >> nv;
+        scanf("%d",&nv);
         int flag = 0;
-        vector<int> hash(m, 0);
+        vector<int> hash(m + 1, 0);
+        for (int j = 0; j < nv; ++j) {
 
-        for (int i = 0; i < nv; ++i) {
-            scanf("%d", &num);
-            // 遍历与该点相关的边 标记
-            for (int l = 0; l < v[num].size(); ++l) {
-                hash[v[num][l]] = 1;
+//            cin >> num;
+            scanf("%d",&num);
+            // 扫边
+            for ( int a : mp[num] ) {
+                hash[a] = 1;
             }
         }
 
-        //  判断 是不是所有的边都有关系
-        for (int j = 0; j < m; ++j) {
+        for (int j = 1; j <= m; ++j) {
             if (hash[j] == 0) {
                 printf("No\n");
                 flag = 1;
                 break;
             }
         }
-
-        if (flag == 0) {
-            printf("Yes\n");
-        }
+        if (flag == 0) printf("Yes\n");
     }
     return 0;
 }
