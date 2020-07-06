1899cc94e6c1d479aa62f7e4f0dc22ebfe8e3170
kelper <644050173@qq.com>
2020-07-07 01:06:06 +0800

    二刷

--- A1149.cpp
+++ A1149.cpp
@@ -1,45 +1,47 @@
-//
-// Created by kelper on 2020/2/8.
-//
+#include <bits/stdc++.h>
 
-
-#include <iostream>
-#include <vector>
-#include <map>
 using namespace std;
 
 int main() {
-    int n, k, t1, t2;
-    map<int, vector<int>> m;
-//    map<int, int> m;
-    cin >> n >> k;
+    int n, m;
+    cin >> n >> m;
+    unordered_map<int, vector<int>> map;
+    int a, b;
     for (int i = 0; i < n; ++i) {
-        cin >> t1 >> t2;
-        m[t1].push_back(t2);
-        m[t2].push_back(t1);
+        cin >> a >> b;
+        map[a].push_back(b);
+        map[b].push_back(a);
     }
 
-    while (k--) {
-        int cnt, flag = 0, a[100009] = {0};
-        cin >> cnt;
-        vector<int> v(cnt);
-        for (int i = 0; i < cnt; ++i) {
-            cin >> v[i];
-            //
-            a[v[i]] = 1;
+    int k;
+    for (int i = 0; i < m; ++i) {
+        cin >> k;
+        vector<int> v(k);
+        unordered_map<int, int> f;
+        for (int j = 0; j < k; ++j) {
+            cin >> v[j];
+            f[v[j]] = 1;
         }
+        int flag = 0;
 
-
-        for (int i = 0; i < v.size(); ++i) {
-            for (int j = 0; j < m[v[i]].size(); ++j) {
-                if (a[m[v[i]][j]] == 1) {
+        for (int l = 0; l < k; ++l) {
+            // 扫表肯定跑不掉
+            for (auto it : map[v[l]]) {
+                // 怎么判断这个东西是不是在这个集合里面
+                // 空间换时间
+                if (f[it] == 1) {
                     flag = 1;
+                    break;
                 }
             }
         }
-//        cout << flag ? "No" : "Yes";
-        printf("%s\n",flag ? "No" :"Yes");
-    }
+        // 不能在判断后输出
+        if (flag == 0) {
+            printf("Yes\n");
+        } else {
+            printf("No\n");
+        }
 
+    }
     return 0;
 }
