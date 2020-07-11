d02e7d5a8ee1395cf17efc0f75b7b7d925266fe3
kelper <644050173@qq.com>
2020-07-11 16:56:46 +0800

    二刷

--- A1146.cpp
+++ A1146.cpp
@@ -9,6 +9,8 @@ int main() {
     cin >> n >> m;
     map<int, vector<int>> v;
     vector<int> in(n + 1);
+
+
     int a, b;
     for (int i = 1; i <= m; ++i) {
         cin >> a >> b;
@@ -30,6 +32,7 @@ int main() {
         // 拷贝
         for (int j = 1; j <= n; ++j) {
             tin[j] = in[j];
+
         }
 
         int v1;
@@ -37,18 +40,30 @@ int main() {
         // 必须把数读完
         for (int j = 1; j <= n; ++j) {
             cin >> v1;
-            // 入度为0 且 没有
+            // 入度为0 且 没有Kkk;
+
             // 判断条件写的复杂了
             // 判断不是 比这个容易 这个判断还容易写错
             // tin 和 in可能写混
-            if (tin[v1] == 0 && flag == 0) {
+//            if (tin[v1] == 0 && flag == 0) {
+//                for (auto it : v[v1]) {
+//                    // v1 指向的点
+//                    // 入度减一
+//                    tin[it]--;
+//                }
+//            } else {
+//                flag = 1;
+//            }
+
+            // 入度如果不为0 不能被输出
+            if (tin[v1] != 0) {
+                flag = 1;
+            } else {
                 for (auto it : v[v1]) {
                     // v1 指向的点
                     // 入度减一
                     tin[it]--;
                 }
-            } else {
-                flag = 1;
             }
         }
 
