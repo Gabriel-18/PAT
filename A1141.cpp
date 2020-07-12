1654e811bb12f26f9beaf159a5e093e7707a293b
kelper <644050173@qq.com>
2020-07-12 23:52:35 +0800

    二刷

--- A1141.cpp
+++ A1141.cpp
@@ -1,23 +1,18 @@
-//
-// Created by kelper on 2020/3/22.
-//
+#include <bits/stdc++.h>
 
-#include <algorithm>
-#include <cctype>
-#include <unordered_map>
-#include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 struct node {
     string school;
     int tws, ns;
 };
-//Then output the ranklist of institutions in nondecreasing order of their ranks in the following format: TWS
-// The institutions are ranked according to their TWS. If there is a tie, the institutions are supposed to have the same rank,
-// and they shall be printed in ascending order of Ns. If there is still a tie, they shall be printed in alphabetical order of their codes.
+string up(string s) {
+    for (int i = 0; i < s.size(); ++i) {
+        s[i] = tolower(s[i]);
+    }
+    return s;
+}
+
 bool cmp(node a, node b) {
-    // decreasing
     if (a.tws != b.tws) {
         return a.tws > b.tws;
     } else if (a.ns != b.ns) {
@@ -29,49 +24,42 @@ bool cmp(node a, node b) {
 int main() {
     int n;
     cin >> n;
-    unordered_map<string, int> cnt;
-    unordered_map<string, double> sum;
-//    ID Score School
+    map<string, int> count;
+    map<string, double> tws;
+    string id, school;
+    double score;
     for (int i = 0; i < n; ++i) {
-        string id, school;
-        double score;
-        cin >> id;
-        cin >> score;
-//        scanf("%lf", &score);
-        cin >> school;
-//        all in lower case
-        for (int j = 0; j < school.length(); ++j) {
-            school[j] = tolower(school[j]);
-        }
-//        TWS is the total weighted score
-//        which is defined to be the integer part of
-//        ScoreB/1.5 + ScoreA + ScoreT*1.5
-        if (id[0] == 'B') {
-            score /= 1.5;
+        cin >> id >> score >> school;
+        school = up(school);
+        count[school]++;
+        if (id[0] == 'A') {
+            tws[school] += score;
         } else if (id[0] == 'T') {
-            score *= 1.5;
+            tws[school] += 1.5 * score;
+        } else if (id[0] == 'B') {
+            tws[school] += score / 1.5;
         }
-        sum[school] += score;
-        cnt[school] ++;
     }
+
     vector<node> ans;
-    for (auto it = cnt.begin(); it != cnt.end(); it++) {
-        ans.push_back(node{it->first,
-                           (int)sum[it->first],
-                           cnt[it->first]}
-                           );
+    for (auto it : tws) {
+        ans.push_back(node {
+            it.first,
+            (int)it.second,
+            count[it.first]
+        });
+
     }
+
     sort(ans.begin(), ans.end(), cmp);
-    int rank = 0, pre = -1;
-    printf("%d\n", ans.size());
+    printf("%d\n",ans.size());
+    int rank = 0;
     for (int i = 0; i < ans.size(); ++i) {
-        if (pre != ans[i].tws) {
-            rank = i + 1;
+        if (ans[i].tws != ans[rank].tws) {
+            rank = i;
         }
-        pre = ans[i].tws;
-        printf("%d ", rank);
-        cout << ans[i].school;
-        printf(" %d %d\n", ans[i].tws, ans[i].ns);
+        printf("%d %s %d %d\n",rank + 1, ans[i].school.c_str(), ans[i].tws, ans[i].ns);
     }
+
     return 0;
 }
